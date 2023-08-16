e0fb8b6165246100aef961695292ff75d81266fd
SwastikRawat11 <117726112+SwastikRawat11@users.noreply.github.com>
2023-08-16 19:30:09 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,12 @@
+(function () {
+ var firebaseConfig = {
+    apiKey: "AIzaSyCKwFLepahWW7o2KzulELn0Adsi6C60UiA",
+    authDomain: "feedbackform-9df57.firebaseapp.com",
+    projectId: "feedbackform-9df57",
+    storageBucket: "feedbackform-9df57.appspot.com",
+    messagingSenderId: "681922973908",
+    appId: "1:681922973908:web:b9be4c6e128aaa5885ab41",
+    measurementId: "G-23J6PQCFTF"
+};
+    firebase.initializeApp(firebaseConfig);
+}());
