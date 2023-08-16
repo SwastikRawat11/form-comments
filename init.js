bb6ce8c637777e05cb50e285071b0c587f299d76
SwastikRawat11 <117726112+SwastikRawat11@users.noreply.github.com>
2023-08-16 20:10:33 +0530

    Update init.js

--- init.js
+++ init.js
@@ -2,12 +2,11 @@
  var firebaseConfig = {
   apiKey: "AIzaSyDomK_Yo8JRm18pt8PQSON81ztXPcgtWCE",
   authDomain: "formdatabase-65d2f.firebaseapp.com",
-  databaseURL: "https://swastikrawat11.github.io/form-comments/",
+  databaseURL: "https://formdatabase-65d2f-default-rtdb.firebaseio.com",
   projectId: "formdatabase-65d2f",
   storageBucket: "formdatabase-65d2f.appspot.com",
   messagingSenderId: "649133372490",
-  appId: "1:649133372490:web:55d93e6f86124f6d2c22b3",
-  measurementId: "G-1CYF7G1Q35"
+  appId: "1:649133372490:web:cc640594ee5528dd2c22b3",
 };
     firebase.initializeApp(firebaseConfig);
 }());
